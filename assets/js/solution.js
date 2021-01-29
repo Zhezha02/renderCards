4111b521c41dfa49ff9000546d49a6f9176e9a5d
Evgeniya Kirtoka <evgeniyakirtoka@gmail.com>
2021-01-29 18:44:51 +0200

    Initial commit with task

---
+++ assets/js/solution.js
@@ -0,0 +1,6 @@
+
+new URL('https://www.facebook.com/JasonStatham/');
+new Map()
+  .set('www.facebook.com', 'src_to_icon')
+  .set('www.facebook.com', 'src_to_icon')
+  .set('www.facebook.com', 'src_to_icon');

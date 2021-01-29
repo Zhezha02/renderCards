fd72caf8cb70d0c65fa6143fafb5c57ca5210807
Evgeniya Kirtoka <evgeniyakirtoka@gmail.com>
2021-01-29 20:41:10 +0200

    Add creation card

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,6 +1,34 @@
+'use strict';
+// new URL('https://www.facebook.com/JasonStatham/');
+// new Map()
+//   .set('www.facebook.com', 'src_to_icon')
+//   .set('www.facebook.com', 'src_to_icon')
+//   .set('www.facebook.com', 'src_to_icon');
+const USER_DESCRIPTION = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim amet perferendis est dolores eum alias.';
 
-new URL('https://www.facebook.com/JasonStatham/');
-new Map()
-  .set('www.facebook.com', 'src_to_icon')
-  .set('www.facebook.com', 'src_to_icon')
-  .set('www.facebook.com', 'src_to_icon');
+
+const userCardContainer = document.getElementById('userCardContainer');
+const userCards = responseData.map((user) => createUsersCards(user));
+userCardContainer.append(...userCards);
+
+function createUsersCards(user) {
+  const card = createElement('article');
+  const userName = createElement('h3', { classNames: ['userName'] }, getFullName(user));
+  const userDescription = createElement('p', {classNames:['cardDescriction']}, USER_DESCRIPTION);
+
+  return card;
+}
+
+function getFullName({ firstName, lastName }) {
+  return `${firstName} ${lastName}`;
+}
+
+function
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+function createElement(type, { classNames, onClick }, ...children) {
+  const elem = document.createElement(type);
+  elem.classList.add(...classNames);
+  elem.onclick = onClick;
+  elem.append(...children);
+  return elem;
+}
